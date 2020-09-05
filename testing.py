f7854b9be3d3423a5b1503639a99ecdbb510e270
shaunacp <60153964+shaunacp@users.noreply.github.com>
2020-09-04 21:30:58 -0500

    Updated testing.py
    Updated version of testing.py with changes.

--- testing.py
+++ testing.py
@@ -1,5 +1,5 @@
 from random import randint
 n = randint(0,1)
-print(f"Am I the master? {'Yes' if n == 1 else 'No'}")
+print(f"Am I the master? {'Yes. I am the master.' if n == 1 else 'No. I am not the master.'}")
 for i in range(1,11):
 	print(f'We are square: {i*i}')
